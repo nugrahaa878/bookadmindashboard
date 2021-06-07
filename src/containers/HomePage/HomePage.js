fde444fab1582a6e4ead710ca9b9f91835fbff09
Ari Nugraha <nugrahaa878@gmail.com>
2021-06-07 13:18:56 +0700

    implement save data from api to reducer

--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
 import BookCard from "../../components/BookCard/BookCard";
 import Header from "../../components/Header/Header";
+import { getAllBook } from "../../store/actions/books";
 import HomePageStyled from "./style";
 
-export default function HomePage() {
-    return (
-        <HomePageStyled>
+class HomePage extends Component {
+
+    componentDidMount() {
+        this.props.getAllBook();
+    }
+
+    render() {
+        return (
+            <HomePageStyled>
             <Header />
             <div className="container">
                 <p>Books (54)</p>
@@ -15,5 +23,25 @@ export default function HomePage() {
             <BookCard />
             <BookCard />
         </HomePageStyled>
-    );
+        )
+    }
 }
+
+const mapStateToProps = (state) => {
+    return {
+        isLoading: state.listBookReducer.isLoading,
+        listBook: state.listBookReducer.listBook
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        getAllBook: () => dispatch(getAllBook())
+    }
+}
+
+export function createHomePage(injectMapDispatchToProps) {
+    return connect(mapStateToProps, injectMapDispatchToProps)(HomePage);
+}
+
+export default createHomePage(mapDispatchToProps);
