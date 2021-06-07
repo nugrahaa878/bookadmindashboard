e37ce57b1db535f021b4273b320790baf8786097
Ari Nugraha <nugrahaa878@gmail.com>
2021-06-07 13:43:01 +0700

    finish implement get all book

--- src/components/BookCard/BookCard.js
+++ src/components/BookCard/BookCard.js
@@ -2,42 +2,44 @@ import React from "react";
 import BookCardStyled from "./style";
 import poster from '../../assets/poster.jpeg';
 
-export default function BookCard() {
+const BookCard = (props) => {
     return (
         <BookCardStyled>
             <div className="grid-container">
                 <div className="left">
                     <div className="title-container">
-                        <h2>Fleishman Is in Trouble</h2>
-                        <p>Book by Ari Angga Nugraha</p>
+                        <h2>{props.title}</h2>
+                        <p>Book by {props.author}</p>
                     </div>
                     <div className="desc-container">
                         <div className="desc-left">
                             <div className="detail">
                                 <p className="title">ISBN</p>
-                                <p>978-123-2-1231-123</p>
+                                <p>{props.isbn}</p>
                             </div>
                             <div className="detail">
                                 <p className="title">Published on</p>
-                                <p>January 30 2021</p>
+                                <p>{props.publishedOn}</p>
                             </div>
                         </div>
                         <div className="desc-right">
                             <div className="detail">
                                 <p className="title">Number of Page</p>
-                                <p>288 Pages</p>
+                                <p>{props.page} Pages</p>
                             </div>
                             <div className="detail">
                                 <p className="title">Country Publisher</p>
-                                <p>U.S</p>
+                                <p>{props.country}</p>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div className="right">
-                    <img src={poster} className="poster" alt="Poster Buku"/>
+                    <img src={props.imgUrl} className="poster" alt="Poster Buku"/>
                 </div>
             </div>
         </BookCardStyled>
     );
 }
+
+export default BookCard;
--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -6,42 +6,60 @@ import { getAllBook } from "../../store/actions/books";
 import HomePageStyled from "./style";
 
 class HomePage extends Component {
+  componentDidMount() {
+    this.props.getAllBook();
+  }
 
-    componentDidMount() {
-        this.props.getAllBook();
+  render() {
+    var isLoading = this.props.isLoading;
+    var listBook = this.props.listBook;
+    var bookCards = [];
+
+    if (!isLoading && listBook) {
+      for (let idx = 0; idx < listBook.length; idx++) {
+        console.log("Hey" + listBook[idx].title);
+        bookCards.push(
+          <BookCard
+            title={listBook[idx].title}
+            author={listBook[idx].author}
+            isbn={listBook[idx].isbn}
+            page={listBook[idx].numberOfPages}
+            publishedOn={listBook[idx].publishedOn}
+            country={listBook[idx].country}
+            imgUrl={listBook[idx].imageUrl}
+          />
+        );
+      }
     }
 
-    render() {
-        return (
-            <HomePageStyled>
-            <Header />
-            <div className="container">
-                <p>Books (54)</p>
-                <button>Add +</button>
-            </div>
-            <BookCard />
-            <BookCard />
-            <BookCard />
-        </HomePageStyled>
-        )
-    }
+    return (
+      <HomePageStyled>
+        <Header />
+        <div className="container">
+          <p>Books (54)</p>
+          <button>Add +</button>
+        </div>
+        {bookCards}
+      </HomePageStyled>
+    );
+  }
 }
 
 const mapStateToProps = (state) => {
-    return {
-        isLoading: state.listBookReducer.isLoading,
-        listBook: state.listBookReducer.listBook
-    }
-}
+  return {
+    isLoading: state.listBookReducer.isLoading,
+    listBook: state.listBookReducer.listBook,
+  };
+};
 
 const mapDispatchToProps = (dispatch) => {
-    return {
-        getAllBook: () => dispatch(getAllBook())
-    }
-}
+  return {
+    getAllBook: () => dispatch(getAllBook()),
+  };
+};
 
 export function createHomePage(injectMapDispatchToProps) {
-    return connect(mapStateToProps, injectMapDispatchToProps)(HomePage);
+  return connect(mapStateToProps, injectMapDispatchToProps)(HomePage);
 }
 
 export default createHomePage(mapDispatchToProps);
